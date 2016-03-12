77a818691b02d97994fd0c4f75c57af8bb5314b7
cmwu <gaminwoo@163.com>
2016-03-12 22:52:42 +0800

    add bootstrap and change the menu

--- MHCartoon/scripts/content.js
+++ MHCartoon/scripts/content.js
@@ -1,29 +1,48 @@
 
 
-	var t = n = 0, count;
-	$(document).ready(function(){	
-		count=$("#pic_list a").length;
-		$("#pic_list a:not(:first-child)").hide();
-		$("#pic_info").html($("#pic_list a:first-child").find("img").attr('alt'));
-		$("#pic_info").click(function(){window.open($("#pic_list a:first-child").attr('href'), "_blank")});
-		$("#pic li").click(function() {
-			var i = $(this).text() - 1;
-			n = i;
-			if (i >= count) return;
-			$("#pic_info").html($("#pic_list a").eq(i).find("img").attr('alt'));
-			$("#pic_info").unbind().click(function(){window.open($("#pic_list a").eq(i).attr('href'), "_blank")})
-			$("#pic_list a").filter(":visible").fadeOut(500).parent().children().eq(i).fadeIn(1000);
-			document.getElementById("banner").style.background="";
-			$(this).toggleClass("on");
-			$(this).siblings().removeAttr("class");
+var t = n = 0, count;
+$(document).ready(function(){	
+	count=$("#pic_list a").length;
+	$("#pic_list a:not(:first-child)").hide();
+	$("#pic_info").html($("#pic_list a:first-child").find("img").attr('alt'));
+	$("#pic_info").click(function(){window.open($("#pic_list a:first-child").attr('href'), "_blank")});
+	$("#pic li").click(function() {
+	var i = $(this).text() - 1;
+	n = i;
+		if (i >= count) return;
+		$("#pic_info").html($("#pic_list a").eq(i).find("img").attr('alt'));
+		$("#pic_info").unbind().click(function(){window.open($("#pic_list a").eq(i).attr('href'), "_blank")})
+		$("#pic_list a").filter(":visible").fadeOut(500).parent().children().eq(i).fadeIn(1000);
+		document.getElementById("banner").style.background="";
+		$(this).toggleClass("on");
+		$(this).siblings().removeAttr("class");
 		});
 		t = setInterval("showAuto()", 4000);
 		$("#pic").hover(function(){clearInterval(t)}, function(){t = setInterval("showAuto()", 4000);});
 	})
-	
-	function showAuto()
+function showAuto()
 	{
-		n = n >=(count - 1) ? 0 : ++n;
-		$("#pic li").eq(n).trigger('click');
+	n = n >=(count - 1) ? 0 : ++n;
+	$("#pic li").eq(n).trigger('click');
 	}
+
+
+
+$(function () {  
+    $("#searched_content").val("请输入关键词").addClass("waiting")  
+    .blur(function () {  
+        if ($(this).val() == "") {  
+            $("#searched_content").val("请输入关键词").addClass("waiting");  
+            }  
+        })  
+    .focus(function () {  
+        if ($("#searched_content").val() == "请输入关键词") {  
+            $("#searched_content").val("").removeClass("waiting");  
+            }  
+     });  
+});  
+
+
+
+
 
